32d55d16e93778639976a6a2ae0b69111bdc7aee
Ivan Egorov <vngrv555@gmail.com>
2024-02-05 09:15:41 +0700

    02-2: Implement pick()

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,8 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
+  const objEntries = Object.entries(obj);
+  const pickedObjEntires = objEntries.filter(([key]) => fields.includes(key));
 
+  return Object.fromEntries(pickedObjEntires);
 };
