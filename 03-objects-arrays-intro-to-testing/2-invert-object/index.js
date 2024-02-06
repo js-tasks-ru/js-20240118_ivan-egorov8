db5cfc7b637d553f32142943d2d5838e1fbe5fca
Ivan Egorov <vngrv555@gmail.com>
2024-02-06 21:09:48 +0700

    03-2: Implement invertObj

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,6 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  const reversedEntries = Object.entries(obj).map((i) => i.reverse());
+  return Object.fromEntries(reversedEntries);
 }
