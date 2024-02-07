b6cb2849b736c279799d7768cccd2d4b0a177738
Ivan Egorov <vngrv555@gmail.com>
2024-02-07 08:04:25 +0700

    03-3: Implement trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,25 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  // массив массивов с повторяющимися символами
+  // Например, для "xxxaxxx" будет [["x", "x", "x"], ["a"], ["x", "x", "x"]]
+  const repeatedChars = [];
 
+  string.split('').forEach((char, idx, chars) => {
+    const previousChar = chars[idx - 1];
+    if (char !== previousChar) {
+      repeatedChars.push([]);
+    }
+    repeatedChars.at(-1).push(char);
+  });
+
+  // массив со строками, обрезанными по допустимому размеру
+  const trimedChars = repeatedChars.map((chars) => {
+    if (chars.length > size) {
+      chars.length = size;
+    }
+    return chars.join('');
+  });
+
+  return trimedChars.join('');
 }
